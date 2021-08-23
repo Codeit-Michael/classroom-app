a59f0f133b11d5ccac6c6d9e73ec1dd3d9de4401
Michael Maranan <michaelmaranan030@gmail.com>
2021-08-23 14:49:00 +0800

    finished views

--- classroom/studentlist/urls.py
+++ classroom/studentlist/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-	path('',views.first,name='first')
-	# path('',views.first.as_view()),
+	path('',views.students),
+	path('view_student/<int:id>',views.view_student),
+	path('add_student/',views.add_student),
+	path('remove_student/<int:id>',views.remove_student),
+	path('update_student/<int:id>',views.update_student),
+	# path('',views.first.as_view()),	# for class based views
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- classroom/studentlist/views.py
+++ classroom/studentlist/views.py
@@ -1,22 +1,63 @@
+# required
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from .serializers import studentSerializer
-from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from django.http import HttpResponse,JsonResponse
 from rest_framework.response import Response
 from .models import student
+from .serializers import studentSerializer
 
-# Create your views here.
+# for function based views
+from rest_framework.decorators import api_view
+
+# for class based views
+from rest_framework.views import APIView
+from rest_framework import status
+
+
+Create your views here.
+
 @api_view(['GET'])
-def first(response):
+def students(request):
 	st = student.objects.all()
 	srl = studentSerializer(st,many=True)
 	return Response(srl.data)
 
-# not working
+@api_view(['GET'])
+def view_student(request,id):
+	st = student.objects.get(id=id)
+	srl = studentSerializer(st,many=False)
+	return Response(srl.data)
+
+@api_view(['POST'])
+def add_student(request,id):
+	srl = studentSerializer(data=request.data)
+	if srl.is_valid():
+		srl.save()
+	return Response(srl.data)
+
+@api_view(['DELETE'])
+def remove_student(request,id):
+	st = student.objects.get(id=id)
+	st.delete()
+	return Response('Student Removed')
+
+@api_view(['POST'])
+def update_student(request,id):
+	st = student.objects.get(id=id)
+	srl = studentSerializer(instance=st,data=request.data)
+	if srl.is_valid():
+		srl.save()
+	return Response(srl.data)
+
+# class based views
 # class first(APIView):
-# 	def get(self,request,format=None):
-# 		queryset = student.objects.all()
-# 		serializer = studentSerializer(student,many=True)
+# 	def get(self,request):
+# 		sets = student.objects.all()
+# 		serializer = studentSerializer(sets,many=True)
 # 		return Response(serializer.data)
+
+# 	def post(self,request):
+# 		serializer = studentSerializer(data = request.data)
+# 		if serializer.is_valid():
+# 			serializer.save()
+# 			return Response(serializer.data, status=status.HTTP_201_CREATED)
+# 		return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
