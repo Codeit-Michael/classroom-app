0549d384d422e62392e71836fdcb7b8313b2a8fd
Michael Maranan <michaelmaranan030@gmail.com>
2021-08-25 12:19:15 +0800

    new app

---
+++ schoolform/registration/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+GENDER_CHOICE = (
+	('M','Male')
+	('F','Female')
+)
+
+COURSE_CHOICE = (
+	('BSBA','BSBA'),
+	('BSIT','BSIT'),
+	('BS-EDUC','BS-EDUC'),
+	('BS-CRIM','BS-CRIM'),
+)
+
+# Create your models here.
+class applicant(models.Model):
+	firstname = models.CharField(max_length=15)
+	lastname = models.CharField(max_length=15)
+	gender = models.CharField(max_length=10,choices=GENDER_CHOICE)
+	phone = models.IntegerField(max_length=11)
+	guardian = models.CharField(max_length=25)
+	guardians_phone = models.IntegerField(max_length=11)
+	address = models.CharField(max_length=60)
+	course = models.CharField(max_length=30,choices=COURSE_CHOICE)
