b81621841d9139a0c59e15588f94ec063ccd5c4d
Michael Maranan <michaelmaranan030@gmail.com>
2021-08-29 17:11:04 +0800

    update models

--- schoolform/registration/models.py
+++ schoolform/registration/models.py
@@ -14,11 +14,16 @@ COURSE_CHOICE = (
 
 # Create your models here.
 class applicant(models.Model):
+	username = models.CharField(max_length=50)
 	firstname = models.CharField(max_length=15)
 	lastname = models.CharField(max_length=15)
 	gender = models.CharField(max_length=10,choices=GENDER_CHOICE)
 	phone = models.IntegerField()
+	email = models.CharField(max_length=70)
 	guardian = models.CharField(max_length=25)
 	guardians_phone = models.IntegerField()
-	address = models.CharField(max_length=60)
+	address = models.CharField(max_length=200)
 	course = models.CharField(max_length=30,choices=COURSE_CHOICE)
+
+	def __str__(self):
+		return self.username
