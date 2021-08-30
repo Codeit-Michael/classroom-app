64a49778dd05c3f45c134956362e4edaef01a496
Michael Maranan <michaelmaranan030@gmail.com>
2021-08-30 11:50:48 +0800

    update forms to modelforms

--- schoolform/registration/forms.py
+++ schoolform/registration/forms.py
@@ -1,22 +1,15 @@
-from django import forms
+from django.forms import ModelForm
 
 # needed for sign ups
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from .models import GENDER_CHOICE,COURSE_CHOICE,applicant
+from .models import applicant
 
-class applicantForm(forms.Form):
-	username = forms.CharField(max_length=50)
-	firstname = forms.CharField(max_length=15)
-	lastname = forms.CharField(max_length=15)
-	gender = forms.ChoiceField(choices=GENDER_CHOICE)
-	phone = forms.IntegerField()
-	email = forms.CharField(max_length=70)
-	guardian = forms.CharField(max_length=25)
-	guardians_phone = forms.IntegerField()
-	address = forms.CharField(max_length=200)
-	course = forms.ChoiceField(choices=COURSE_CHOICE)
+class applicantForm(ModelForm):
+	class Meta:
+		model = applicant
+		fields = '__all__'
 
 class CreateUserForm(UserCreationForm):
 	class Meta:
