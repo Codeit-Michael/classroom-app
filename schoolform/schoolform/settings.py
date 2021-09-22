7a26e8f39c4a0348d47d02e8466e614a0a2b0d9e
Michael Maranan <michaelmaranan030@gmail.com>
2021-09-22 23:32:51 +0800

    static files

--- schoolform/schoolform/settings.py
+++ schoolform/schoolform/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import os
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / "static",
+    '/var/www/static/',
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
